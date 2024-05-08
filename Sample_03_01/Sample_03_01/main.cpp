861d885900341182b89de85767c469adf3779320
Jun Shimura <junshimura@gmail.com>
2024-05-08 17:30:33 +0900

    Matrix

--- Sample_03_01/Sample_03_01/main.cpp
+++ Sample_03_01/Sample_03_01/main.cpp
@@ -10,66 +10,79 @@ void InitRootSignature(RootSignature& rs);
 ///////////////////////////////////////////////////////////////////
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
 {
-    //ゲームの初期化
-    InitGame(hInstance, hPrevInstance, lpCmdLine, nCmdShow, TEXT("Game"));
+	//ゲームの初期化
+	InitGame(hInstance, hPrevInstance, lpCmdLine, nCmdShow, TEXT("Game"));
 
-    //////////////////////////////////////
-    // ここから初期化を行うコードを記述する
-    //////////////////////////////////////
+	//////////////////////////////////////
+	// ここから初期化を行うコードを記述する
+	//////////////////////////////////////
 
-    //ルートシグネチャを作成
-    RootSignature rootSignature;
-    InitRootSignature(rootSignature);
+	//ルートシグネチャを作成
+	RootSignature rootSignature;
+	InitRootSignature(rootSignature);
 
-    //三角形ポリゴンを定義
-    TrianglePolygon triangle;
-    triangle.Init(rootSignature);
+	//三角形ポリゴンを定義
+	TrianglePolygon triangle;
+	triangle.Init(rootSignature);
 
-    // step-1 定数バッファを作成
+	// step-1 定数バッファを作成
+	ConstantBuffer cb;
+	cb.Init(sizeof(Matrix)); // Init関数の引数は定数バッファーのサイズ
 
-    // step-2 ディスクリプタヒープを作成
+	// step-2 ディスクリプタヒープを作成
+	DescriptorHeap ds;
+	ds.RegistConstantBuffer(0, cb); // ディスクリプタヒープに定数バッファーを登録
+	ds.Commit();                    // ディスクリプタヒープへの登録を確定
 
-    //////////////////////////////////////
-    // 初期化を行うコードを書くのはここまで！！！
-    //////////////////////////////////////
-    auto& renderContext = g_graphicsEngine->GetRenderContext();
+	//////////////////////////////////////
+	// 初期化を行うコードを書くのはここまで！！！
+	//////////////////////////////////////
+	auto& renderContext = g_graphicsEngine->GetRenderContext();
 
-    // ここからゲームループ
-    while (DispatchWindowMessage())
-    {
-        //フレーム開始
-        g_engine->BeginFrame();
+	float f = 0;
+	// ここからゲームループ
+	while (DispatchWindowMessage())
+	{
+		f++;
+		//フレーム開始
+		g_engine->BeginFrame();
 
-        //////////////////////////////////////
-        //ここから絵を描くコードを記述する
-        //////////////////////////////////////
+		//////////////////////////////////////
+		//ここから絵を描くコードを記述する
+		//////////////////////////////////////
 
-        //ルートシグネチャを設定
-        renderContext.SetRootSignature(rootSignature);
+		//ルートシグネチャを設定
+		renderContext.SetRootSignature(rootSignature);
 
-        // step-3 ワールド行列を作成
+		// step-3 ワールド行列を作成
+		Matrix mWorld1;
+		mWorld1.MakeTranslation(sin(f / 100.0f), 0.0f, 0.0f);
+		Matrix mWorld2;
+		mWorld2.MakeRotationZ(f / 100.0f);
+		Matrix mWorld = mWorld2 * mWorld1;
+		// step-4 ワールド行列をグラフィックメモリにコピー
+		cb.CopyToVRAM(mWorld);
 
-        // step-4 ワールド行列をグラフィックメモリにコピー
+		// step-5 ディスクリプタヒープを設定
+		renderContext.SetDescriptorHeap(ds);
 
-        // step-5 ディスクリプタヒープを設定
+		//三角形をドロー
+		triangle.Draw(renderContext);
 
-        //三角形をドロー
-        triangle.Draw(renderContext);
-
-        /// //////////////////////////////////////
-        //絵を描くコードを書くのはここまで！！！
-        //////////////////////////////////////
-        //フレーム終了
-        g_engine->EndFrame();
-    }
-    return 0;
+		/// //////////////////////////////////////
+		//絵を描くコードを書くのはここまで！！！
+		//////////////////////////////////////
+		//フレーム終了
+		g_engine->EndFrame();
+	}
+	return 0;
 }
 
 //ルートシグネチャの初期化
-void InitRootSignature( RootSignature& rs )
+void InitRootSignature(RootSignature& rs)
 {
-    rs.Init(D3D12_FILTER_MIN_MAG_MIP_LINEAR,
-        D3D12_TEXTURE_ADDRESS_MODE_WRAP,
-        D3D12_TEXTURE_ADDRESS_MODE_WRAP,
-        D3D12_TEXTURE_ADDRESS_MODE_WRAP);
+	rs.Init(D3D12_FILTER_MIN_MAG_MIP_LINEAR,
+		D3D12_TEXTURE_ADDRESS_MODE_WRAP,
+		D3D12_TEXTURE_ADDRESS_MODE_WRAP,
+		D3D12_TEXTURE_ADDRESS_MODE_WRAP);
 }
